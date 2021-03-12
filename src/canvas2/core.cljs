1f1657126ff8b4e1ea0ea0a40c42047d1d8258ab
Danurai <Danurai@users.noreply.github.com>
2021-03-12 17:23:12 +0000

    Update core.cljs

--- src/canvas2/core.cljs
+++ src/canvas2/core.cljs
@@ -1,32 +1,60 @@
-(ns canvas2.core
-  )
+(ns rathree.core
+  (:require cljsjs.three))
 
+(def canvas   (.querySelector js/document "#c"))
+(def renderer (js/THREE.WebGLRenderer. (clj->js { :canvas canvas :antialias true})))
+(def scene    (js/THREE.Scene.))
+(def camera   (let [fov 60 aspectratio (/ 16 9) near 1 far 1000]
+  (js/THREE.PerspectiveCamera. fov aspectratio near far) ))
+(def light (let [colour 0xFFFFFF intensity 1]
+  (js/THREE.DirectionalLight. colour intensity)))
+
+
+(defn on-window-resize [ evt ]
+  (let [ width (.-innerWidth js/window) height (.-innerHeight js/window) ]
+    (set! (.-aspect camera) (/ width height))
+    (.updateProjectionMatrix camera)
+    (.setSize renderer width height )
+    ))
+
+(def loader (js/THREE.CubeTextureLoader.))
+(def skyboxtexture 
+  (.load loader ["img/skybox/posx.png" "img/skybox/negx.png"
+                 "img/skybox/posy.png" "img/skybox/negy.png"
+                 "img/skybox/posz.png" "img/skybox/negz.png"]))
 
 (defn main [& args]
-  (let [canvas (.querySelector js/document "#c")
-        renderer (js/THREE.WebGLRenderer. (clj->js {:canvas canvas}))
-        ;width (.-clientWidth canvas)
-        ;height (.-clientHeight canvas)
-        width (.-innerWidth js/window)
-        height (.-innerHeight js/window)
-        scene    (js/THREE.Scene.)
-        camera   (js/THREE.PerspectiveCamera. 75 (/ width height) 0.1 100) 
+  (let [width    (.-innerWidth js/window)
+        height   (.-innerHeight js/window) 
         geometry (js/THREE.BoxGeometry.)
         material (js/THREE.MeshPhongMaterial. (clj->js {:color 0x55ddee}))
         cube     (js/THREE.Mesh. geometry material)
-        light    (js/THREE.DirectionalLight. 0xFFFFFF 1)
         animate  (fn cb [] 
                   (js/requestAnimationFrame cb)
                   (set! (.. cube -rotation -x) (+ 0.01 (.. cube -rotation -x)))
                   (set! (.. cube -rotation -y) (+ 0.02 (.. cube -rotation -y)))
                   (.render renderer scene camera))
       ]
-  (.setSize renderer width height)
-  (.add scene cube)
-  (.add scene light)
-  (set! (.. camera -position -z) 5)
-  (.set (.. light -position) -2 4 4)
-  (animate)
-))
+    (.setSize renderer width height)
+    ;(aset camera "aspect" (/ width height))
+    (set! (.-aspect camera) (/ width height))
+    (.updateProjectionMatrix camera)
+
+    (set! (.-background scene) skyboxtexture) ;(js/THREE.Color. 0xAAAAFF))
+    
+    (.add scene cube)
+    (.add scene light)
+    (.add scene (js/THREE.AmbientLight. 0x101010))
+    (.set (.. camera -position) 0 0 10)
+    (.set (.. light -position) 20 100 10)
+    (.set (.. light -target -position) 0 0 0)
+
+    (animate)
+    (.addEventListener js/window "resize" on-window-resize)
+  )
+)
+
+
+
 
 (main)
